762ba6c30a0064c3161c38f311b0327787d0461b
Daniel Ramos Martinez <daniel.martinez@globant.com>
2022-10-26 20:22:24 -0700

    GuestBook gradle

---
+++ src/test/java/com/example/guestbook/GuestbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.guestbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GuestbookApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
